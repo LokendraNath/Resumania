18ca0af141406a0db5bdaa86ffc0f4a2fb70fb1c
Lokendra Nath <nathdeveloper25@gmail.com>
2025-10-14 22:03:13 +0530

    create backend part

---
+++ server/controllers/userControllers.js
@@ -0,0 +1,72 @@
+import User from "../modals/userModal.js";
+import jwt from "jsonwebtoken";
+
+// Create Token JWT
+const generateToken = (userId) => {
+  return jwt.sign({ id: userId }, process.env.JWT_SECRET, { expiresIn: "7d" });
+};
+
+export const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const userExist = await User.findOne({ email });
+    if (userExist) {
+      return res.status(400).json({ message: "User Already Exist" });
+    }
+
+    // Hash password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id),
+    });
+  } catch (error) {
+    res.send(500).json({ message: "Server Error", error: error.message });
+  }
+};
+
+export const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(500).json({ message: "Invalid Email or Password" });
+    }
+
+    // Check Password
+    const isMatch = await bcrypt.compare(password, user._id);
+    if (!isMatch) {
+      return res.status(500).json({ message: "Invalid Email or Password" });
+    }
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id),
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Server Error", error: error.message });
+  }
+};
+
+export const getUserProfile = async (req, res) => {
+  try {
+    const user = await User.findOne(req.user.id).select("-password");
+    if (!user) {
+      return res.status(500).json({ message: "User Not Found" });
+    }
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ message: "Server Error", error: error.message });
+  }
+};
--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
+import { connectDB } from "./config/db.js";
 
 const app = express();
 const PORT = 4000;
@@ -8,6 +9,7 @@ const PORT = 4000;
 app.use(cors());
 
 // Connect DB
+connectDB();
 
 // Middleware
 app.use(express.json());
